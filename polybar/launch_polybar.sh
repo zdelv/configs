ebbab066cdaab6090bb447ab673bb6cde9247802
Zach DelVecchio <zach@zdelvecch.io>
2024-03-13 17:28:51 -0400

    Delete everything and start over

--- polybar/launch_polybar.sh
+++
@@ -1,7 +0,0 @@
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload -c ~/etc/polybar.new test &
-  done
-else
-  polybar --reload -c ~/etc/polybar.new test &
-fi
