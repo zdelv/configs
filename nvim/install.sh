ae6466772badeaeac3bc79bfae482ed62620250c
Zach DelVecchio <zach@zdelvecch.io>
2024-03-22 19:28:50 -0400

    Added neovim specific directory and install
    Adds neovim plugins directory. This allows for neovim specific
    plugins. Includes an install script for easily installing the
    files to an output directory.

---
+++ nvim/install.sh
@@ -0,0 +1,63 @@
+# CLI Interface
+# -o, --output-dir: output directory, default=~/.vim
+# -h, --help: help
+# -v, --version: version
+
+FILES=(".vimrc" "lua/")
+
+VERSION="1.0.0"
+
+# Copy files in current directory
+# to output directory
+function copy_files() {
+	# Create output directory if it does not exist
+	if [ ! -d $OUTPUT_DIR ]; then
+		mkdir -p $OUTPUT_DIR
+	fi
+
+	# Copy files
+	for file in ${FILES[@]}; do
+		printf "Copying $file to $OUTPUT_DIR\n"
+		cp -r $file $OUTPUT_DIR
+	done
+}
+
+# Print help
+function print_help() {
+	echo "Usage: ./install.sh [options]"
+	echo "Options:"
+	echo "  -o, --output-dir, default=~/.vim"
+	echo "  -h, --help"
+	echo "  -v, --version"
+}
+
+# Print version
+function print_version() {
+	echo "Version: $VERSION"
+}
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+	key="$1"
+	case $key in
+		-o|--output-dir)
+			OUTPUT_DIR="$2"
+			shift
+			shift
+			;;
+		-h|--help)
+			print_help
+			exit 0
+			;;
+		-v|--version)
+			print_version
+			exit 0
+			;;
+		*)
+			echo "Unknown option: $1"
+			exit 1
+			;;
+	esac
+done
+
+copy_files
---
+++ nvim/lua/float-terminal.lua
@@ -0,0 +1,109 @@
+local api = vim.api
+
+local buf, win
+local terminal_open = false
+
+local function create_terminal()
+    buf = api.nvim_create_buf(false, true)
+    api.nvim_buf_set_option(buf, 'buflisted', false)
+    api.nvim_buf_set_option(buf, 'swapfile', false)
+    api.nvim_buf_set_option(buf, 'modifiable', true)
+end
+
+local function open_terminal()
+    if not buf then
+        create_terminal()
+    end
+
+    local width = api.nvim_get_option("columns")
+    local height = api.nvim_get_option("lines")
+    local win_height = math.ceil(height * 0.4 - 4)
+    local win_width = math.ceil(width * 0.8)
+    local row = math.ceil((height - win_height) / 2 - 1)
+    local col = math.ceil((width - win_width) / 2)
+
+    local opts = {
+        style = "minimal",
+        relative = "editor",
+        width = win_width,
+        height = win_height,
+        row = row,
+        col = col,
+        border = "rounded"
+    }
+
+    win = api.nvim_open_win(buf, true, opts)
+    api.nvim_win_set_option(win, 'winhl', 'Normal:Normal')
+    api.nvim_win_set_option(win, 'winblend', 10)
+    api.nvim_buf_set_option(buf, 'modified', false)
+
+    -- Set the 'buflisted' option to false for the buffer
+    api.nvim_buf_set_option(buf, 'buflisted', false)
+
+    -- Create an autocommand to toggle the floating window when switching to a different window
+    local cmd = string.format(
+        [[autocmd WinEnter * ++nested if win_getid() != %d && nvim_win_is_valid(%d) | execute "FloatTerminalToggle" | endif]],
+        win,
+        win
+    )
+    vim.cmd(cmd)
+
+    -- Open the terminal in the floating window's buffer
+    if not terminal_open then
+        vim.fn.termopen(vim.o.shell, {buffer = buf})
+        terminal_open = true
+    end
+
+    vim.cmd('startinsert')
+end
+
+local function keep_cursor_in_window()
+    if win and api.nvim_win_is_valid(win) then
+        api.nvim_set_current_win(win)
+    end
+end
+
+local function close_terminal()
+    api.nvim_win_hide(win)
+end
+
+local function toggle_terminal()
+    if win and api.nvim_win_is_valid(win) then
+        if vim.fn.pumvisible() == 1 then
+            vim.cmd('close')
+        end
+        api.nvim_win_hide(win)
+    else
+        if not buf then
+            create_terminal()
+        end
+        open_terminal()
+    end
+end
+
+local function delete_terminal()
+    if buf and api.nvim_buf_is_valid(buf) then
+        api.nvim_buf_delete(buf, {force = true})
+        buf = nil
+        win = nil
+        terminal_open = false
+        print('Deleted terminal')
+    end
+end
+
+-- Define user commands
+api.nvim_create_user_command('FloatTerminal', open_terminal, {})
+api.nvim_create_user_command('FloatTerminalToggle', toggle_terminal, {})
+api.nvim_create_user_command('FloatTerminalDelete', delete_terminal, {})
+
+-- Define keymappings
+local opts = { noremap = true, silent = true }
+api.nvim_set_keymap('n', '<leader><leader>t', '<cmd>FloatTerminalToggle<cr>', opts)
+api.nvim_set_keymap('n', '<leader><leader>d', '<cmd>FloatTerminalDelete<cr>', opts)
+api.nvim_set_keymap('t', '<esc>', '<C-\\><C-n><cmd>FloatTerminalToggle<cr>', opts)
+
+-- Export the module
+return {
+    toggle_terminal = toggle_terminal,
+    keep_cursor_in_window = keep_cursor_in_window
+}
